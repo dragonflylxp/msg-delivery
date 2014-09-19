03f5bf6f1ea33dff5fe52e00377801e6619a46a4
dragonflylxp <dragonflylxp@gmail.com>
2014-09-19 18:00:28 +0800

    tornado framework

--- bin/main.py
+++ bin/main.py
@@ -4,4 +4,17 @@
 #Date: 2014-09-19 17:13:40
 #Desc: 
 
-print "hello world"
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world")
+        
+application = tornado.web.Application([(r"/",MainHandler),])
+
+if __name__ == "__main__":
+    application.listen(8776)
+    tornado.ioloop.IOLoop.instance().start()
+    
